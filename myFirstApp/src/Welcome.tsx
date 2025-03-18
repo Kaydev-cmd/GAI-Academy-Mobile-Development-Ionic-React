195fbca035d5da09ae291f8b7ef622b38dbed7c7
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-03-18 15:42:26 -0700

    first commit

--- myFirstApp/src/Welcome.tsx
+++ myFirstApp/src/Welcome.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonPage,
+  IonInput,
+  IonButton,
+} from "@ionic/react";
+import { useState } from "react";
 
-const Welcome = () => {
+const Welcome: React.FC = () => {
+  const [username, setUsername] = useState("Keamogetswi");
+  const [message, setMessage] = useState("Welcome to Bootcamp!");
+
   return (
-    <div>Welcome</div>
-  )
-}
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Welcome {username}</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent
+        className="ion-padding"
+        style={{ backgroundColor: "#f4f4f4" }}
+      >
+        <h1>{message}</h1>
 
-export default Welcome
+        {/* Input field to change the username */}
+        <IonInput
+          placeholder="Enter your name"
+          onIonChange={(e) => setUsername(e.detail.value!)}
+          clearInput
+        />
+
+        {/* Button to update message */}
+        <IonButton
+          expand="full"
+          onClick={() => setMessage("Let's start learning!")}
+        >
+          Update Message
+        </IonButton>
+        <footer>Day 1 Assignment</footer>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Welcome;
